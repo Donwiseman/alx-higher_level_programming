e515c95ced2b02ce6fc344c424bc0c451133ec1c
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-05-10 13:39:28 -0700

    11-pow.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def pow(a, b):
+    num = 1
+    if b == 0:
+        return 1
+    if b < 0:
+        for x in range((b * -1)):
+            num *= a
+        num = 1/num
+    else:
+        for x in range(b):
+            num *= a
+    return num
+
+
+print(pow(4, 5))
+print(pow(-4, 5))
+print(pow(2, -2))
