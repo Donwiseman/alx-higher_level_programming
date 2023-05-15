4d65c81cbc0575cbcd5f9e7b667f9455514afc2a
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-05-15 13:27:17 -0700

    7-add_tuple.py update not using unpacking

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,9 +7,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 1:
         x, y = tuple_a[0], 0
     else:
-        x, y = tuple_a
+        x, y = tuple_a[0], tuple_a[1]
     if len(tuple_b) == 1:
         a, b = tuple_b[0], 0
     else:
-        a, b = tuple_b
+        a, b = tuple_b[0], tuple_b[1]
     return ((x + a), (y + b))
