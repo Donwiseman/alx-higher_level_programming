2cedbe1ce51e7b4dcce10f574070e1e5f7c6d39e
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-05-15 09:42:47 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print()
+        return
+    for row in matrix:
+        for index, value in enumerate(row):
+            if (index + 1) == len(row):
+                print("{:d}".format(value))
+                break
+            print("{:d} ".format(value), end="")
