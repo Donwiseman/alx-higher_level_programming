e8675a16210d6fcddd964e4fd911b01d67c96551
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-05-16 18:51:50 -0700

    3-print_reversed_list_integer.py update to cover fo a None case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """prints a list of integers in reverse to stdout"""
+    if my_list is None:
+        return
     if len(my_list) > 0:
         for value in my_list[-1::-1]:
             print("{:d}".format(value))
