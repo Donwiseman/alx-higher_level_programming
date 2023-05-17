4623015ab09f097c79987b7e98f6bd72a073bacf
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-05-16 19:38:28 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,10 +9,38 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int *int_arr[], index;
+	int *arr, index = 0, count, last_index;
+	listint_t *current = *head;
 
 	if ((head == NULL) || (*head == NULL))
 		return (1);
-	if (*head->next == NULL)
-		return (0)
+	if (current->next == NULL)
+		return (0);
+	arr = (int *) malloc(1 * sizeof(int));
+	if (arr == NULL)
+		exit(EXIT_FAILURE);
+	while (current != NULL)
+	{
+		arr[index] = current->n;
+		if (current->next != NULL)
+		{
+			index++;
+			arr = (int *) realloc(arr, (index + 1) * sizeof(int));
+			if (arr == NULL)
+				exit(EXIT_FAILURE);
+		}
+		current = current->next;
+	}
+	last_index = index;
+	for (count = 0; count < ((index + 1) / 2); count++)
+	{
+		if (arr[count] != arr[last_index])
+		{
+			free(arr);
+			return (0);
+		}
+		last_index--;
+	}
+	free(arr);
+	return (1);
 }
