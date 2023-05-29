a8eee19650a24715ed09fe189c98e5614fc893b8
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-05-29 09:10:59 -0700

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    if x == 0:
+        return 0
+    for value in range(0, x):
+        try:
+            print("{:d}".format(my_list[value]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    if count != 0:
+        print()
+    return count
