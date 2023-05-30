da2b95b281687655ab510cee657ca3a83cd519f3
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-05-30 14:07:14 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square(object):
+    """A class that performs various actions related to a square"""
+    def __init__(self, size):
+        """ instantiation of the square class
+
+        Args:
+            __size (int): size of the square
+
+        """
+        self.__size = size
