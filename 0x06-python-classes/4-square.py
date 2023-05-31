d939bacbb3362bf64f889644296a76d734c4c4ec
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-05-30 22:01:12 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""defines a square by: (based on 3-square.py) and adds property for attr"""
+
+
+class Square(object):
+    """A class that performs various actions related to a square"""
+    def __init__(self, size=0):
+        """ instantiation of the square class
+
+        Args:
+            size (int): size of the square
+
+        """
+        self.__size = size
+
+    def area(self):
+        """calculates the current area of the square
+
+        Returns:
+            int: area of the square presntly
+
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """returnsthe value of the square size
+
+        setter methos raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (type(value) != int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
