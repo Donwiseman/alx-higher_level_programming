32f5ace14d686746a2c4ca4ef69509b2ed255440
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-06-01 08:29:00 -0700

    0-safe_print_list.py new correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
 
     if x == 0:
+        print()
         return x
     for value in my_list:
         try:
@@ -12,6 +13,5 @@ def safe_print_list(my_list=[], x=0):
                 break
         except Exception:
             pass
-    if count != 0:
-        print()
+    print()
     return count
