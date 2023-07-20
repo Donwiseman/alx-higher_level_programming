e67c99262e01b0251c3ed0dab5ae615bc730a7c6
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-07-20 15:38:14 -0700

    4-first_table.sql

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- Lists all the tables of a database in your MySQL server.
+-- Command to list all tables in database passed as argument.
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a new table in a given database
+-- Command to create a new table
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256)
+)
