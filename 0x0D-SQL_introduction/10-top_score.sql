ba4b657460eba7d304e6841e7c8a7565dc5c68e1
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-07-21 03:25:55 -0700

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table of the database hbtn_0c_0
+-- Code using SELECT to retrieve data
+SELECT score, name
+FROM second_table
+ORDER BY score DESC, name ASC;
