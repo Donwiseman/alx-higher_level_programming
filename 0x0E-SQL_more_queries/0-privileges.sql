c57b2c1810f6686900abd0958bfd27ba265e6e22
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-06 07:04:24 -0700

    0-privileges.sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- This shows the privileges available for user_0d_1 and user_0d_2
+-- Below is  the code to request for the user privileges on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SJOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+s repo contains more SQL queries involving establishing new users and granting access, use of joins and others.
