7a88ab2f6fab48c59578ab7b1eee2001de24a69a
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-07 06:15:49 -0700

    1-create_user.sql correction

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- This creates the user user_od_1 and grants it all privileges.
 -- The below codes carries out the above.
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS
+	'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
