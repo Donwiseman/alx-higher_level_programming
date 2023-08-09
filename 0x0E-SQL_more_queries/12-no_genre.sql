a19924530a41b56a9884bb71d7f5fd5bc30c3aa0
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-09 05:38:00 -0700

    12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows without genre id.
+-- Uses SELECT with JOIN statement to lists them.
+SELECT title, genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id is NULL
+ORDER BY title ASC, genre_id ASC;
