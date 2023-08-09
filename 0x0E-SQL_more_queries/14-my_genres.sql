09fb9cd8cd280a5d14d247252fec4a0d379e188b
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-09 14:10:24 -0700

    14-my_genres.sql correction

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,8 +3,6 @@
 SELECT name
 FROM tv_genres INNER JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.genre_id
-WHERE show_id = (
-	SELECT id
-	FROM tv_shows
-	WHERE title = 'Dexter'
-);
+	INNER JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+WHERE title = 'Dexter';
