82042c83f0c0dbd8710bc404f989aa68d3ac513f
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-18 05:13:31 -0700

    1-filter_states.py correction closed conn

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ This is a script that uses MySQLdb to lists all states from
-the database hbtn_0e_0_usa.
+the database hbtn_0e_0_usa that starts with `N`.
 """
 
 import sys
@@ -17,3 +17,5 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
