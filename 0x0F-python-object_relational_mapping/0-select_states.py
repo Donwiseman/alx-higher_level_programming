fc9ff7209ea4b8b2d2e93121318222541113bb48
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-18 05:52:12 -0700

    added port declaration

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ import MySQLdb
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         exit()
-    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
