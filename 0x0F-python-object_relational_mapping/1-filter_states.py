80ca4e71ca5f2061ad2cd17281a90ee96fe0c741
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-18 05:15:15 -0700

    1-filter_states.py correction removed validation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,6 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        exit()
     db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
