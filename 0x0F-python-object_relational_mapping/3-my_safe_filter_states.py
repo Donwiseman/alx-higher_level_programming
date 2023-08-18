99cfe53693478b9bb0bdef74641c2003191efc2e
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-18 09:18:26 -0700

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This is a script that uses MySQLdb to lists all states from
+the database hbtn_0e_0_usa that matches the passes argument keyword
+and provides protection against SQL injection attacks.
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
