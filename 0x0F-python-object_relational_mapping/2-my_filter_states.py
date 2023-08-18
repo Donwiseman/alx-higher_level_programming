b85e09fbb38b14b0a10b3163be2e7c78f4912e05
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-18 05:38:02 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This is a script that uses MySQLdb to lists all states from
+the database hbtn_0e_0_usa that matches the passes argument keyword.
+"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(
+            sys.argv[4])
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
