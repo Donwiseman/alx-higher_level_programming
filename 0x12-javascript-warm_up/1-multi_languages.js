949070fcafa746418bb5030b861312fb2b12c9bb
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-08-27 11:31:46 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Prints C is fun, python is cool and JavaScript is amazing in console
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
