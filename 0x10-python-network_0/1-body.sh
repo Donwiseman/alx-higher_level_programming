91202ff8bd2ac8c27846cc1eb60ff76a776b2665
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-08-31 19:36:26 -0700

    1-body.sh update using --fail

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays the body of the response
-STATUS=$(curl -s -o /dev/null -w '%{http_code}' "$1")
-if [ "$STATUS" -eq 200 ]; then
-	curl -s "$1" | tr -d '\n'
-fi
+curl -s --fail "$1" | tr -d '\n'
