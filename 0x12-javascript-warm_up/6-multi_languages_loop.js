889ebe9b4ab41dc9f3e51c6ed14f876073d8f5a7
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-09-14 09:02:04 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Prints C is fun, python is cool and JavaScript is amazing in console
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let x = 0; x < 3; x++) {
+  console.log(arr[x]);
+}
